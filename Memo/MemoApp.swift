b5914629578a4db66e0e164e765d985beea088d1
markusniv <markus.nivasalo@outlook.com>
2022-04-04 11:58:38 +0300

    Initial Commit

---
+++ Memo/MemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by iosdev on 4.4.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
