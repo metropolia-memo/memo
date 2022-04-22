f146e960e6fbf2bbdcdceb10741343b25d0bc542
sirjak <sirja.kosonen@gmail.com>
2022-04-22 14:33:21 +0300

    Added DrawerMenu UI

---
+++ Memo/Components/DrawerMenu/DrawerMenuView.swift
@@ -0,0 +1,97 @@
+//
+//  DrawerMenuView.swift
+//  Memo
+//
+//  Created by Sirja Kosonen on 20.4.2022.
+//
+
+import SwiftUI
+
+struct DrawerMenuView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            
+            // Profile area
+            VStack(alignment: .leading) {
+                Button(action: {}) {
+                    Image(systemName: "person.circle.fill")
+                    // TODO: Delete foregroundColor when image changed to profile photo
+                        .foregroundColor(Color.black)
+                        .frame(width: 50, height: 50)
+                        .scaleEffect(3)
+                }
+                
+                Text("John Doe")
+                    .padding(.top, 25)
+                    .foregroundColor(Color.white)
+                Text("john.doe@gmail.com")
+                    .font(.subheadline)
+                    .accentColor(Color.white)
+            }
+            .padding(.top, 30)
+            .padding(10)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color(red: 124/255, green: 153/255, blue: 255/255))
+            
+            // Home button
+            HStack {
+                Button(action: {}) {
+                    Image(systemName: "house.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .foregroundColor(Color.black)
+                        .frame(width: 25, height: 25)
+                    Text("Home")
+                        .font(.headline)
+                        .foregroundColor(Color.black)
+                }
+            }
+            .padding(.top, 30)
+            .padding(10)
+            
+            // Profile button
+            HStack {
+                Button(action: {}) {
+                    Image(systemName: "person.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .foregroundColor(Color.black)
+                        .frame(width: 25, height: 25)
+                    Text("Profile")
+                        .font(.headline)
+                        .foregroundColor(Color.black)
+                }
+            }
+            .padding(10)
+            
+            // History button
+            HStack {
+                Button(action: {}) {
+                    Image(systemName: "clock")
+                        .resizable()
+                        .scaledToFill()
+                        .foregroundColor(Color.black)
+                        .frame(width: 25, height: 25)
+                    Text("History")
+                        .font(.headline)
+                        .foregroundColor(Color.black)
+                    
+                }
+            }
+            .padding(10)
+            Divider()
+                .background(Color.black)
+                .padding(.top, 30)
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color(red: 242/255, green: 242/255, blue: 242/255))
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct DrawerMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        DrawerMenuView()
+    }
+}
